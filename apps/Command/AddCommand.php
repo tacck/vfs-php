cdea039f8d5b5a5cb1f4230d5a4c947a7547b9dc
Takuya KIHARA <gray@tacck.net>
2018-04-28 22:55:11 +0900

    :sparkles: CommandBuilderからAddCommand呼出のテストと初期実装。

---
+++ apps/Command/AddCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Command;
+
+/**
+ * [Command] ファイル追加処理を行なうクラス
+ *
+ * 指定されたファイルを、仮想ファイルシステムへ追加する。
+ *
+ * @package App\Command
+ */
+class AddCommand implements CommandInterface
+{
+
+    public function exec(): bool
+    {
+        // TODO: Implement exec() method.
+        return true;
+    }
+}
